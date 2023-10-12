e753ba7c8f7d436b0af8fc7e7a66a31f2f07201d
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-10-12 21:44:28 +0200

    working on sticky navigation with observer API

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
+'use strict'
+
+const nav = document.querySelector('.nav--menu');
+const sectionAboutUs = document.querySelector('.about_us--container');
 const allSections = document.querySelectorAll('.section');
+
+const initialCoords = sectionAboutUs.getBoundingClientRect();
+
+window.addEventListener('scroll', function(event){
+    console.log(window.scrollY);
+
+    if(window.scrollY > initialCoords.top) nav.classList.add('sticky') 
+    else nav.classList.remove('sticky');
+})
+
 
 const revealSection = function(entries, observer){
     const [entry] = entries;
