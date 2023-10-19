9f367fc06f83c1ae692a490d4acf1256919b2aaf
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-10-19 17:20:58 +0200

    Still working on lazy images
    Script seems to be working but photos from data-src don't want to load in DOM

--- dist/index.dffb2d51.js
+++ dist/index.dffb2d51.js
@@ -68,16 +68,18 @@ autoSlider();
 const imgTargets = document.querySelectorAll("img[data-src]");
 const loadImg = function(entries, observer) {
     const [entry] = entries;
-    console.log(entry);
     if (!entry.isIntersecting) return;
+    // Replace src with data-src
     entry.target.src = entry.target.dataset.src;
     entry.target.addEventListener("load", function() {
         entry.target.classList.remove("lazy-img");
     });
+    observer.unobserve(entry.target);
 };
 const imgObserver = new IntersectionObserver(loadImg, {
     root: null,
-    threshold: 0
+    threshold: 0,
+    rootMargin: "200px"
 });
 imgTargets.forEach((img)=>imgObserver.observe(img)); /* const showImage = function(index){
     sliderImages.forEach((img, i) => {
--- src/js/main.js
+++ src/js/main.js
@@ -80,19 +80,25 @@ autoSlider();
 
 const imgTargets = document.querySelectorAll('img[data-src]');
 
-const loadImg = function(entries, observer){
-    const [entry] = entries;
-    console.log(entry);
-    if(!entry.isIntersecting) return;
-    entry.target.src = entry.target.dataset.src;
-    entry.target.addEventListener('load', function(){
-        entry.target.classList.remove('lazy-img');
-    });
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
-    root: null,
-    threshold: 0,
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
