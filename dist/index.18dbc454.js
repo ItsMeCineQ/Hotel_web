05c1d69cffb55a54111b773afe54814fb71d610b
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-10-26 22:12:28 +0200

    Cleaned rendercalendar function

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -974,12 +974,29 @@ const dayName = [
     "Sun"
 ];
 const sectionCalendar = document.querySelector(".calendar");
+const firstDayOfMonth = new Date(currentYear, currentMonth, 0).getDay();
+const lastDay = new Date(currentYear, currentMonth + 1, 0).getDate();
+const lastDateOfPreviousMonth = new Date(currentYear, currentMonth, 0).getDate();
+const lastDayOfMonth = new Date(currentYear, currentMonth, firstDayOfMonth).getDay();
+const renderDays = function() {
+    return `
+        ${Array.from({
+        length: firstDayOfMonth
+    }, (_, i)=>`<li class="days_prev_next">${lastDateOfPreviousMonth - i}</li>`).reverse().join("")}
+        ${Array.from({
+        length: lastDay
+    }, (_, i)=>`<li>${i + 1}</li>`).join("")}
+        ${Array.from({
+        length: lastDayOfMonth
+    }, (_, i)=>`<li class="days_prev_next">${i + 1}</li>`).join("")}
+    `;
+};
+const renderDayTags = function() {
+    return `
+        ${dayName.map((day)=>`<li>${day}</li>`).join("")}
+    `;
+};
 const renderCalendar = function() {
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 0).getDay();
-    const lastDay = new Date(currentYear, currentMonth + 1, 0).getDate();
-    const lastDateOfPreviousMonth = new Date(currentYear, currentMonth, 0).getDate();
-    const lastDayOfMonth = new Date(currentYear, currentMonth, firstDayOfMonth).getDay();
-    console.log(lastDay);
     let html = `
         <div class="current_year-month">
             <h2>${months[currentMonth]}, ${currentYear}</h2>
@@ -991,18 +1008,10 @@ const renderCalendar = function() {
             </button>
         </div>
         <ul class="day--name">
-            ${dayName.map((day)=>`<li>${day}</li>`).join("")}
+            ${renderDayTags()}
         </ul>
         <ul class="day--date">
-            ${Array.from({
-        length: firstDayOfMonth
-    }, (_, i)=>`<li class="days_prev_next">${lastDateOfPreviousMonth - i}</li>`).reverse().join("")}
-            ${Array.from({
-        length: lastDay
-    }, (_, i)=>`<li>${i + 1}</li>`).join("")}
-            ${Array.from({
-        length: lastDayOfMonth
-    }, (_, i)=>`<li class="days_prev_next">${i + 1}</li>`).join("")}
+            ${renderDays()}
         </ul>
     `;
     sectionCalendar.insertAdjacentHTML("beforeend", html);
--- src/js/calendar.js
+++ src/js/calendar.js
@@ -6,14 +6,26 @@ const currentMonth = date.getMonth();
 const currentDay = date.getDate();
 const dayName = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 const sectionCalendar = document.querySelector('.calendar');
+const firstDayOfMonth = new Date(currentYear, currentMonth, 0).getDay();
+const lastDay = new Date(currentYear, currentMonth + 1, 0).getDate();
+const lastDateOfPreviousMonth = new Date(currentYear, currentMonth, 0).getDate();
+const lastDayOfMonth = new Date(currentYear, currentMonth, firstDayOfMonth).getDay();
+
+const renderDays = function(){
+    return `
+        ${Array.from({length: firstDayOfMonth}, (_, i) => `<li class="days_prev_next">${lastDateOfPreviousMonth - i}</li>`).reverse().join('')}
+        ${Array.from({length: lastDay}, (_, i) => `<li>${i + 1}</li>`).join('')}
+        ${Array.from({length: lastDayOfMonth}, (_, i) => `<li class="days_prev_next">${i + 1}</li>`).join('')}
+    `
+};
+
+const renderDayTags = function(){
+    return `
+        ${dayName.map(day => `<li>${day}</li>`).join('')}
+    `
+};
 
 export const renderCalendar = function () {
-    const firstDayOfMonth = new Date(currentYear, currentMonth, 0).getDay();
-    const lastDay = new Date(currentYear, currentMonth + 1, 0).getDate();
-    const lastDateOfPreviousMonth = new Date(currentYear, currentMonth, 0).getDate();
-    const lastDayOfMonth = new Date(currentYear, currentMonth, firstDayOfMonth).getDay();
-
-    console.log(lastDay);
     let html =  `
         <div class="current_year-month">
             <h2>${months[currentMonth]}, ${currentYear}</h2>
@@ -25,12 +37,10 @@ export const renderCalendar = function () {
             </button>
         </div>
         <ul class="day--name">
-            ${dayName.map(day => `<li>${day}</li>`).join('')}
+            ${renderDayTags()}
         </ul>
         <ul class="day--date">
-            ${Array.from({ length: firstDayOfMonth }, (_, i) => `<li class="days_prev_next">${lastDateOfPreviousMonth - i}</li>`).reverse().join('')}
-            ${Array.from({length: lastDay}, (_, i) => `<li>${i + 1}</li>`).join('')}
-            ${Array.from({length: lastDayOfMonth}, (_, i) => `<li class="days_prev_next">${i + 1}</li>`).join('')}
+            ${renderDays()}
         </ul>
     `
     sectionCalendar.insertAdjacentHTML('beforeend', html);
